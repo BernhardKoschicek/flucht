e3eb0548a8f9d88b053477d7e19cf26729595305
BernhardKoschicek <b.koschicek@gmail.com>
2021-07-13 19:08:41 +0200

    image text

--- flucht/data/exhibition.py
+++ flucht/data/exhibition.py
@@ -6,37 +6,36 @@ def build_img_tag():
     img_tag = {}
     for name, value in pictures_exhibition.items():
         img_tag[name] = f'''
-<figure>
-    <img id={name} data-bs-toggle="modal" data-bs-target="#{name}Modal" class="img-thumbnail"
-         src="{url_for('static', filename=value['filepath'])}" alt="{value['text']}">
-</figure>
-<div class="modal fade" id="{name}Modal" tabindex="-1" aria-labelledby="{name}Modal"
-     aria-hidden="true">
-    <div class="modal-dialog modal-xl modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-body">
-                <div class="img_container">
-                    <img id={name} class="modal-image" src="{url_for('static',
-                    filename=value['filepath'])}" alt="{value['text']}">
-                </div>
-            </div>
-            <div class="modal-footer justify-content-between">
-                    <div class="col-10">
-                        <p class="pic-source"><a href="{value['url']}">{value['url']}</a>
-                            ({value['licensing']})
-                        </p>
-                        <figcaption>{value['text']}</figcaption>
+            <figure>
+                <img id={name} data-bs-toggle="modal" data-bs-target="#{name}Modal" class="img-thumbnail"
+                     src="{url_for('static', filename=value['filepath'])}" alt="{value['text']}">
+            </figure>
+            <div class="modal fade" id="{name}Modal" tabindex="-1" aria-labelledby="{name}Modal"
+                 aria-hidden="true">
+                <div class="modal-dialog modal-xl modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <div class="img_container">
+                                <img id={name} class="modal-image" src="{url_for('static',
+                                filename=value['filepath'])}" alt="{_(value['text'])}">
+                            </div>
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                                <div class="col-10">
+                                    <p class="pic-source"><a href="{value['url']}">{value['url']}</a>
+                                        ({value['licensing']})
+                                    </p>
+                                    <figcaption>{_(value['text'])}</figcaption>
+                                </div>
+                                <div class="col-1">
+                                    <button type="button" class="btn btn-secondary justify-content-end"
+                                            data-bs-dismiss="modal">Close
+                                    </button>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-1">
-                        <button type="button" class="btn btn-secondary justify-content-end"
-                                data-bs-dismiss="modal">Close
-                        </button>
-                    
                 </div>
-            </div>
-        </div>
-    </div>
-</div> 
+            </div> 
             '''
     return img_tag
 
@@ -56,43 +55,43 @@ licensing = {'public_domain': 'Public Domain'}
 pictures_exhibition = {
     'kundmachung19071915': {
         'filepath': 'images/exhibition/kundmachung19071915.jpg',
-        'text': _('kundmachung19071915'),
+        'text': 'kundmachung19071915',
         'url': 'https://digital.onb.ac.at/rep/access/preview/BAG_14257396',
         'licensing': licensing['public_domain']
     },
     'kuk_maschinengewehr': {
         'filepath': 'images/exhibition/1024px-01915_Maschinengewehr_Abteilung_Lawoczne.jpg',
-        'text': _('maschinengewher_lawoczne'),
+        'text': 'maschinengewher_lawoczne',
         'url': 'https://commons.wikimedia.org/wiki/File:01915_Maschinengewehr_Abteilung,_Lawoczne.jpg',
         'licensing': licensing['public_domain']
     },
     'kundmachung20051916': {
         'filepath': 'images/exhibition/kundmachung20051916.jpg',
-        'text': _('kundmachung20051916'),
+        'text': 'kundmachung20051916',
         'url': 'https://digital.onb.ac.at/rep/access/preview/BAG_14258692',
         'licensing': licensing['public_domain']
     },
     'kriegsschaeden1917': {
         'filepath': 'images/exhibition/kriegsschaeden1917.jpg',
-        'text': _('kriegsschaeden1917'),
+        'text': 'kriegsschaeden1917',
         'url': 'https://digital.onb.ac.at/rep/access/preview/BAG_15568812',
         'licensing': licensing['public_domain']
     },
     'ansicht_oberhollabrunn1910': {
         'filepath': 'images/exhibition/ansicht_oberhollabrunn1910.jpg',
-        'text': _('ansicht_oberhollabrunn1910'),
+        'text': 'ansicht_oberhollabrunn1910',
         'url': 'https://akon.onb.ac.at/#id=AKON_AK063_199',
         'licensing': licensing['public_domain']
     },
     'lagertor': {
         'filepath': 'images/exhibition/lagertor.jpg',
-        'text': _('lagertor'),
+        'text': 'lagertor',
         'url': '',
         'licensing': ''
     },
     'lagertor_oberhollabrunn1916': {
         'filepath': 'images/exhibition/lagertor_oberhollabrunn1916.jpg',
-        'text': _('lagertor_oberhollabrunn1916'),
+        'text': 'lagertor_oberhollabrunn1916',
         'url': 'https://upload.wikimedia.org/wikipedia/de/e/e3/Stockerau_Fl%C3%BCchtlingslager.jpg',
         'licensing': licensing['public_domain']
     }
