3a0f3adbc653b0fe9e57145d9e3fca9943a50444
BernhardKoschicek <b.koschicek@gmail.com>
2021-10-11 09:28:45 +0200

    sort pages

--- flucht/util/pages.py
+++ flucht/util/pages.py
@@ -6,4 +6,4 @@ from flucht import app
 def get_sections_pages():
     path = pathlib.Path(app.root_path) / 'templates/pages'
     files = [f'pages/{x.name}' for x in path.glob('**/*') if x.is_file()]
-    return files
+    return files.sort()
