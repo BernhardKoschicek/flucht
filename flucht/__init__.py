99dbf2aed0ce1953ae619961335e235983d262f0
bkoschicek <b.koschicek@gmail.com>
2026-01-04 17:33:50 +0100

    fix for debian trixie

--- flucht/__init__.py
+++ flucht/__init__.py
@@ -8,16 +8,18 @@ app.config.from_pyfile('production.py')  # type: ignore
 
 # import and register blueprints
 from flucht.blueprints.multilingual import multilingual
+
 app.register_blueprint(multilingual)
 
-babel = Babel(app)
 
-
-@babel.localeselector
 def get_locale():
     if not g.get('lang_code', None):
-        g.lang_code = request.accept_languages.best_match(app.config['LANGUAGES'])
+        g.lang_code = request.accept_languages.best_match(
+            app.config['LANGUAGES'])
     return g.lang_code
+
+
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.before_request
@@ -28,7 +30,8 @@ def before_request():
 
 @app.after_request
 def apply_caching(response):
-    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    response.headers[
+        'Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
     response.headers['X-Content-Type-Options'] = 'nosniff'
     response.headers['X-Frame-Options'] = 'SAMEORIGIN'
     response.headers['X-XSS-Protection'] = '1; mode=block'
